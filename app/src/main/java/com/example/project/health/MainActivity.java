053a9952d6a4b7a74252de4d86fef27edaf370b4
Vinay <vinayviny34@gmail.com>
2017-03-04 18:07:51 +0530

    New commit

--- app/src/main/java/com/example/project/health/MainActivity.java
+++ app/src/main/java/com/example/project/health/MainActivity.java
@@ -18,16 +18,25 @@ public class MainActivity extends AppCompatActivity {
     private EditText username=null;
     private EditText password=null;
     private Button login=null;
+    private TextView text=null;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        username = (EditText)findViewById(R.id.username);
-        password = (EditText)findViewById(R.id.password);
-        login = (Button)findViewById(R.id.button);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.button);
+        text = (TextView) findViewById(R.id.signup);
 
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent j = new Intent(MainActivity.this,Signup_activity.class);
+                startActivity(j);
+            }
+        });
     }
 
     public void login(View view) {
@@ -35,10 +44,15 @@ public class MainActivity extends AppCompatActivity {
                 password.getText().toString().equals("admin")) {
             Toast.makeText(getApplicationContext(), "Welcome",
                     Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(MainActivity.this,Home_activity.class);
+            Intent i = new Intent(MainActivity.this,Home_Acitivity.class);
             startActivity(i);
 
-        } else {
+        }else if((username.getText().toString().equals("")) && (password.getText().toString().equals(""))){
+            Toast.makeText(getApplicationContext(), "No credential",
+                    Toast.LENGTH_SHORT).show();
+
+        }
+        else {
             Toast.makeText(getApplicationContext(), "Invalid username or password",
                     Toast.LENGTH_SHORT).show();
 
